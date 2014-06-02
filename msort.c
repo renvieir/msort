2981bda44b6c1226fa9c40eef4d9d5787b789e2c
Renato Vieira <renato.pacheco.r2@gmail.com>
2014-06-02 16:57:45 -0400

    Modificando variaveis e construindo o bloco da intercalacao

--- msort.c
+++ msort.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* Verifica se os parâmetros foram passados de forma correta ao programa */
 void verificaParametros(int argc);
 int load(char *fileName, int *V,int num,FILE *pFile);
 int write(int *V,int num, char *fileName);
@@ -18,43 +17,38 @@ void print(int *V, int n)
 }
 
 int main( int argc, char** argv )
-{    
+{
     verificaParametros(argc);
 
     FILE *pFile;
     int memoria,kVias;
     char *fileIn,*fileOut;
-    
+
     fileIn = argv[1];
     fileOut = argv[2];
     memoria = atoi(argv[3]);
     kVias = atoi(argv[4]);
-    
-    /*  
-     *  Etapa 1: geração de corridas.
-     *           Apenas um buffer na memoria principal.
-     *  
-     *  Passo 1: Criar um buffer do tamanho <memoria>.
-     *  Passo 2: Encher buffer com dados do arquivo de entrada.
-     *  Passo 3: Ordenar buffer com um algoritmo de ordenação estável.
-     *  Passo 4: Gravar buffer em um arquivo.
-     *  Passo 5: Repetir Passos 2 e 4 até o fim do arquivo de entrada.
+
+    /*
+     *  Etapa 1: geracao de corridas.
      */
 
     int *buffer,qtdElementos,tamElemento;
 
     tamElemento = sizeof(int);
     qtdElementos = memoria/tamElemento;
-    
+
     // Passo 1: Criar um buffer do tamanho <memoria>.
     buffer = (int *) malloc(qtdElementos*tamElemento);
-    
-    int i=0,count =0;
+
+    int qtdCorridas=0,count =0;
     long int endOfFile=0,pointerFile=0;
     char fileTempName[25];
-    
-    // Abre arquivo de entrada para geração das corridas
+
+    // Abre arquivo de entrada para geracao das corridas
     pFile = fopen(fileIn,"r");
+
+    // Pegando fim do arquivo
     fseek(pFile,0,SEEK_END);
     endOfFile = ftell(pFile);
     rewind(pFile);
@@ -62,48 +56,87 @@ int main( int argc, char** argv )
     while(pointerFile < endOfFile)
     {
         // Carrega os arquivos para a memoria
-        printf("--- Carregando dados ---\n");    
+        printf("--- Carregando dados ---\n");
         count = fread(buffer,tamElemento,qtdElementos,pFile);
-        printf("--- numeros lidos %d ---\n",count);    
-        
-        // Ordenação com quicksort
-        printf("--- Ordenando ---\n");    
+        printf("--- numeros lidos %d ---\n",count);
+
+        // Ordenacao com quicksort
+        printf("--- Ordenando ---\n");
         quicksort(buffer,0,count-1);
-        
+
         // Grava uma nova corrida
-        sprintf(fileTempName,"%d.tmp",i);       
-        printf("--- Gerando arquivo %s---\n",fileTempName);    
-        write(buffer,count,fileTempName);        
-        
+        sprintf(fileTempName,"%d.tmp",qtdCorridas);
+        printf("--- Gerando arquivo %s---\n",fileTempName);
+        write(buffer,count,fileTempName);
+
         pointerFile = ftell(pFile);
-        printf("--- Posicao Ponteiro %ld---\n",pointerFile);    
-        
-        i++;
+        printf("--- Posicao Ponteiro %ld---\n",pointerFile);
+
+        qtdCorridas++;
     }
-    
+
     // Fecha o arquivo
     fclose(pFile);
+    free(buffer);
 
     /*
-     *  Etapa 2: intercalação das corridas.
-     *           kVias + 1 buffers na memória principal.
+     *  Etapa 2: intercalacao das corridas.
+     *           kVias + 1 buffers na memoria principal.
      */
      // Passo 1: Encher as kVias com arquivo de corrida.
      // Passo 2: Intercalar kVias no buffer de saida.
      // Passo 3: Se buffer de saida estiver cheio grava num arquivo de saida.
-     // Passo 4: Repetir passos 1 a 4 até os arquivos abertos inicialmente acabarem.
+     // Passo 4: Repetir passos 1 a 4 ate os arquivos abertos inicialmente acabarem.
      // Passo 5: Fechar arquivo de saida gerando uma nova corrida com tamanho kvias vezes maior.
-     // Passo 6: Repetir passo 1 a 5 até sobrar apenas um arquivo.
-    
+     // Passo 6: Repetir passo 1 a 5 ate sobrar apenas um arquivo.
+
+    int **V,*I,tamBuffer,qtdBuffers,i,j,x,y,minX,minY;
+
+    FILE *fSorted;
+
+    qtdBuffers = kVias +1;
     /* Tamanho de cada uma das k vias */
-    //tamBuffer = memoria/qtdBuffers;
-    
-    /* Alocação dos buffers para intercalação */
-    /*int i;
+    tamBuffer = memoria/qtdBuffers;
+
+    /* Alocacao dos buffers para intercalacao */
     for(i=0;i<qtdBuffers;i++)
     {
         V[i] = (int *) malloc(tamBuffer*sizeof(int));
-    }*/
+    }
+
+    I = (int *) malloc(kVias*sizeof(int));
+    for(i=0;i<kVias;i++)
+    {
+	I[i] = 0;
+    }
+
+    while(1)
+    {
+	// carrega buffers
+
+	// intercala
+	for(y=0;y<kVias*tamBuffer;y++)
+	{
+	    minX = 0;
+	    minY = 0;
+	    for(x=0;x<kVias;x++)
+	    {
+		if(I[x]<tamBuffer){
+		    if(V[x,I[x]]<V[minX,minY])
+		    {
+		        minX = x;
+		        minY = I[x];
+		    }
+		}
+	    }
+	    V[kVias] = V[minX,minY];
+	    I[minX]=I[minX] + 1;
+	}
+
+	// grava
+	write(V[kVias],tamBuffer,"nome-provisorio");
+    }
+
 }
 
 void verificaParametros(int argc)
@@ -122,21 +155,21 @@ void verificaParametros(int argc)
 
 int load(char *fileName, int *V,int num, FILE *pFile)
 {
-    return fread(V,sizeof(int),num,pFile);    
+    return fread(V,sizeof(int),num,pFile);
 }
 
 int write(int *V,int num, char *fileName)
 {
     FILE *pFile;
-    
-    pFile = fopen(fileName,"w");    
-    int n = fwrite(V,sizeof(int),num,pFile);    
+
+    pFile = fopen(fileName,"w");
+    int n = fwrite(V,sizeof(int),num,pFile);
     fclose(pFile);
     return n;
 }
 
 
-/************** Ordenação *****************/
+/************** Ordenacao *****************/
 troca(int *a, int *b){
 	int aux;
 	aux=*a;
